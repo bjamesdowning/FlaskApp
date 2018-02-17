ea41a8feeedfef4b3a29beb4de2e0e435f1482d5
bjamesdowning <bjamesdowning@gmail.com>
2018-02-17 18:49:08 +0300

    Python & HTML edit
    added page for 'submit' in __init__.py
    added form to test text input in HTML

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
 #Flask testing
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
 	return render_template("homepage.html")
+
+@app.route('/submit', methods=['POST'])
+	def submit():
+		return 'You entered: {}'.format(request.form['text'])
 
 if __name__ == '__main__':
 	app.run()
