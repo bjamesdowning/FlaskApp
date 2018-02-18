dea825a8c37ae6c702dce73cc6607b2cc8abc52e
bjamesdowning <bjamesdowning@gmail.com>
2018-02-18 22:03:12 +0300

    html

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return render_template("login.html")
+	return render_template("homepage.html")
 
 @app.route('/login', methods=['POST'])
 def login():
